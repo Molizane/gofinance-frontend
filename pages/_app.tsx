01ea297480f232b89c349f409639eb54abd9732d
Molizane <molizane@gmail.com>
2023-05-14 23:19:54 -0300

    feat: theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../styles/globals";
+import { darkTheme } from "@/styles/themes/dark";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ styles/themes/dark.ts
@@ -0,0 +1,14 @@
+export const darkTheme = {
+  title: "dark",
+  colors: {
+    primary: "#FF7A00",
+    secondary: "#FF97#6",
+    tertiary: "#FFBC7D",
+    black1: "#131212",
+    black2: "#171616",
+    black3: "#1E1E1E",
+    white: "#FFF",
+  },
+  spacing: {},
+  sizes: {},
+};
